1cc2cded034109876fc80589a82dbefec13fac02
IosDevelopUZ <126049891+IosDevelopUZ@users.noreply.github.com>
2023-08-21 14:49:54 +0500

    Initial Commit

---
+++ API Calls/API_CallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallsApp.swift
+//  API Calls
+//
+//  Created by Khalmatov on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct API_CallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
